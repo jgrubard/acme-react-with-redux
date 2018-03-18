f6ed2eb6dd173ec9f842dca03677fc14d3df2b4a
Jeremy Grubard <jgrubard@gmail.com>
2018-03-18 17:32:41 -0400

    adding routes

--- server.js
+++ server.js
@@ -15,35 +15,7 @@ app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 });
 
-app.get('/api/users', (req, res, next) => {
-  User.findAll()
-    .then(users => res.send(users))
-    .catch(next);
-});
-
-app.post('/api/users', (req, res, next) => {
-  User.create(req.body)
-    .then(user => res.send(user))
-    .catch(next);
-});
-
-app.put('/api/users/:id', (req, res, next) => {
-  User.findById(req.params.id)
-    .then(user => {
-      Object.assign(user, req.body);
-      return user.save();
-    })
-    .then(user => res.send(user))
-    .catch(next);
-});
-
-app.delete('/api/users/:id', (req, res, next) => {
-  User.findById(req.params.id)
-    .then(user => user.destroy())
-    .then(() => res.sendStatus(204))
-    .catch(next);
-})
-
+app.use('/api/users', require('./routes/users.js'))
 
 const port = process.env.PORT || 3000;
 
